564763b4b1c0acea6f23ba201af95aa97a5406fd
phammanhhung <phammanhhung10501@gmail.com>
2021-02-12 19:38:50 +0700

    Add event to changeSizeButton, make Grid responsive to input

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
 const container = document.querySelector('.container');
+
+function clearGrid()
+{
+    let childList = container.childNodes;
+    while (childList[0] != null)
+    {
+        container.removeChild(childList[0]);
+    }
+}
+
 function createGrid(num) {
     container.style['grid-template-columns'] = `repeat(${num}, 1fr)`;
     for (let i = 0; i < num * num; ++i) {
@@ -8,4 +18,14 @@ function createGrid(num) {
     }
 }
 
-createGrid(5);
+createGrid(4);
+
+document.querySelector('#changeSizeButton').addEventListener('click', () => 
+{
+    const input = document.querySelector('input');
+    let num = Number(input.value);
+    if (num < 1 || num > 64) return;
+    console.log(num);
+    clearGrid();
+    createGrid(num);
+})
